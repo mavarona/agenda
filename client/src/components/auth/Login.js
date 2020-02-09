8d6f541d5f48b74b62d7ce9cf95e3d5933956fe9
mavl <mvaronamac@gmail.coml>
2020-02-09 20:42:20 +0100

    Create component login and signup with states and functions

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [user, saveUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = user;
+
+    const onChangeLogin = e => {
+        saveUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onSubmitLogin = e => {
+        e.preventDefault();
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Acceso a mi agenda</h1>
+                <form
+                    onSubmit={onSubmitLogin}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Correo Electrónico"
+                            value={email}
+                            onChange={onChangeLogin}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Contraseña</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Contraseña"
+                            value={password}
+                            onChange={onChangeLogin}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit" 
+                            className="btn btn-primario btn-block" 
+                            value="Iniciar Sesión"    
+                        />
+                    </div>
+                </form>
+                <Link to={'/signup'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,8 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+    const [user, saveUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmar: ''
+    });
+
+    const { name, email, password, confirmar } = user;
+
+    const onChangeSignup = e => {
+        saveUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onSubmitSignup = e => {
+        e.preventDefault();
+    }
+
     return (
-        <h1>Crear Cuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Obtener una cuenta</h1>
+                <form
+                    onSubmit={onSubmitSignup}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="name">Nombre</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Tu nombre"
+                            value={name}
+                            onChange={onChangeSignup}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Correo Electrónico"
+                            value={email}
+                            onChange={onChangeSignup}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Contraseña</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Contraseña"
+                            value={password}
+                            onChange={onChangeSignup}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Confirmar Contraseña</label>
+                        <input
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            placeholder="Repite la contraseña"
+                            value={confirmar}
+                            onChange={onChangeSignup}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit" 
+                            className="btn btn-primario btn-block" 
+                            value="Crear Cuenta"    
+                        />
+                    </div>
+                </form>
+                <Link to={'/'} className="enlace-cuenta">
+                    Volver a iniciar sesión
+                </Link>
+            </div>
+        </div>
     )
 }
 
