3bf88aa6358c993bbc2dbdd90a967eedba42f499
mavl <mvaronamac@gmail.coml>
2020-02-17 11:50:53 +0100

    Add context task

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,20 @@ import Signup from './components/auth/Signup';
 import Projects from './components/projects/Projects';
 
 import ProjectState from './context/projects/projectState';
+import TaskState from './context/tasks/taskState';
 
 function App() {
   return (
     <ProjectState>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/projects" component={Projects} />
-        </Switch>
-      </Router>
+      <TaskState>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/projects" component={Projects} />
+          </Switch>
+        </Router>
+      </TaskState>
     </ProjectState>
   );
 }
