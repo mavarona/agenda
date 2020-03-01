615f747fbaba87f321e38281ba8e6f702b18f096
mavl <mvaronamac@gmail.coml>
2020-03-01 12:43:48 +0100

    Add get projects for a creator

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,6 +1,12 @@
 const Project = require('../models/Project');
+const { validationResult } = require('express-validator');
 
 exports.createProject = async(req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
 
     try {
         const project = new Project(req.body);
@@ -10,6 +16,18 @@ exports.createProject = async(req, res) => {
     } catch (err) {
         console.log('Error create a project: ', err);
         res.send(500).json({ msg: 'Error al crear un proyecto' });
+    }
+
+}
+
+exports.getProjects = async(req, res) => {
+
+    try {
+        const projects = await Project.find({ creator: req.user.id }).sort({ created: -1 });
+        res.json(projects);
+    } catch (err) {
+        console.log('Error create a project: ', err);
+        res.send(500).json({ msg: 'Error al obtener los proyectos' });
     }
 
 }
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
-router.post('/', auth, projectController.createProject);
+router.post('/',
+    auth, [
+        check('name', 'hay que darle un nombre al proyecto').not().isEmpty()
+    ],
+    projectController.createProject);
+
+router.get('/',
+    auth,
+    projectController.getProjects);
 
 module.exports = router;
